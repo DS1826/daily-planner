b1fa394195c0657d117e67b4fdecabfb08bece52
Dina Saunders <dina_saunders@mac.com>
2020-09-15 10:06:14 -0400

    add items to local storage function and updated relevant html id attr

--- assets/script.js
+++ assets/script.js
@@ -6,23 +6,51 @@ $(document).ready(function () {
     $("#currentDay").text(today);
 
     // Current Hour stores the integer for the current hour
-    const currentHour = parseInt(moment().format("hA"));
+    const currentHour = parseInt(moment().format("kA"));
     console.log(currentHour);
 
     // Each function will iterate over each textarea id to compare time block integer to current hour integer
-    $("textarea").each(function () {
+    $(".description").each(function () {
         var id = parseInt($(this).attr("id"));
         console.log("Time Block Hour = " + id);
         console.log("Current Hour = " + currentHour);
         
         if (id < currentHour) {
-            $(".description").addClass("future");
+            $(this).addClass("past");
         } else if (id > currentHour) {
-            $(".description").addClass("past");
+            $(this).addClass("future");
         } else {
-            $(".description").addClass("present");
+            $(this).addClass("present");
         }
 
+    });
+
+    // var text9am = document.querySelector("#text-9am").textContent;
+    // console.log(text9am);
+
+    // Click function saves text input to local storage
+    $("button").click(function () {
+        console.log("save button has been clicked");
+        var textInput9 = $("#text-9am").val();
+        var textInput10 = $("#text-10am").val();
+        var textInput11 = $("#text-11am").val();
+        var textInput12 = $("#text-12pm").val();
+        var textInput13 = $("#text-13pm").val();
+        var textInput14 = $("#text-14pm").val();
+        var textInput15 = $("#text-15pm").val();
+        var textInput16 = $("#text-16pm").val();
+        var textInput17 = $("#text-17pm").val();
+        
+        localStorage.setItem("9AM", textInput9);
+        localStorage.setItem("10AM", textInput10);
+        localStorage.setItem("11AM", textInput11);
+        localStorage.setItem("12PM", textInput12);
+        localStorage.setItem("1PM", textInput13);
+        localStorage.setItem("2PM", textInput14);
+        localStorage.setItem("3PM", textInput15);
+        localStorage.setItem("4PM", textInput16);
+        localStorage.setItem("5PM", textInput17);
+        
     });
 
   });
